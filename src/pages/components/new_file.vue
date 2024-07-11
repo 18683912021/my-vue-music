f29e5a87195ef9b58063372f84104b8b5f2dab69
wuyang <1161547386@qq.com>
2024-07-11 09:54:53 +0800

    fix

---
+++ src/pages/components/new_file.vue
@@ -0,0 +1,17 @@
+<template>
+    <view>
+      <text>{{ message }}</text>
+    </view>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const message = ref('Hello, Uni-app with Vue 3!');
+  </script>
+  
+  <style scoped>
+  text {
+    font-size: 16px;
+  }
+  </style>
