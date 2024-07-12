869dac7f1adae06e993602e35d27d6db0c4e2c21
wuyang <1161547386@qq.com>
2024-07-12 16:25:46 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import {
 } from "vue";
 import store from './store'
 import App from "./App.vue";
+import '@/pages/assets/scss/index.scss'//引入全局样式
 export function createApp() {
 	const app = createSSRApp(App);
 	app.use(store)
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,4 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
 </template>
 
 <script>
