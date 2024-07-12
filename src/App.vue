1601b5955f470c534721733d2a3e2131e6cf36e2
wuyang <1161547386@qq.com>
2024-07-12 16:49:09 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
+<template>
+  <m-header />
+</template>
 <script>
 export default {
   onLaunch: function () {
-    console.log('App Launch')
+    // console.log('App Launch')
   },
   onShow: function () {
-    console.log('App Show')
+    // console.log('App Show')
   },
   onHide: function () {
-    console.log('App Hide')
+    // console.log('App Hide')
   },
+  components: {
+    MHeader: () => import('@/pages/components/header/header.vue')
+  }
 }
 </script>
 
