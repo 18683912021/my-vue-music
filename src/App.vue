52106fe003da0b66e0033a0b6b53bd45e6a08b74
wuyang <1161547386@qq.com>
2024-08-22 15:33:27 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <m-header></m-header>
+  <Tab />
 </template>
 
 <script>
 import Header from '@/components/header/header'
+import Tab from '@/components/tab/tab'
 
 export default {
   components: {
-    MHeader: Header
+    MHeader: Header,
+    Tab: Tab
   },
   computed: {
 
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="tab">
+        <router-link class="tab-item" v-for="tab in tabs">
+            <span class="tab-link">
+                {{ tab.name }}
+            </span>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: 'tab',
+    data() {
+        return {
+            tabs: [
+                { name: '推荐', path: '/recommend' },
+                { name: '歌手', path: '/singer' },
+                { name: '排行', path: '/rank' },
+                { name: '搜索', path: '/search' }
+            ]
+        }
+    }
+}
+</script>
+<style lang="scss" scoped></style>
