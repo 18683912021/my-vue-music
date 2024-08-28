cd6d047c3821b15d7a19772093012eec63093bb9
wuyang <1161547386@qq.com>
2024-08-28 08:56:47 +0800

    fix

--- src/components/base/scroll/use-scroll.js
+++ src/components/base/scroll/use-scroll.js
@@ -2,6 +2,7 @@ import BScroll from "@better-scroll/core";
 import ObserveDOM from "@better-scroll/observe-dom";
 import { onMounted, onUnmounted,ref } from "vue"
 
+BScroll.use(ObserveDOM)
 
 export default function useScroll(wrapperRef,options) {
   const scroll = ref(null)
